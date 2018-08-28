4a6938bea56c78e3fa2a0255b9a0fd1b8758a957
bkell11 <bkeller11@msn.com>
2018-08-27 23:05:07 -0600

    scrape

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
-// /* global bootbox */
-var Article = require("./models/Article");
+
 $(document).ready(function () {
     //     // Setting a reference to the article-container div where all the dynamic content will go
     //     // Adding event listeners to any dynamically generated "save article"
     //     // and "scrape new article" buttons
     //     var articleContainer = $(".article-container");
-    $(document).on("click", ".save-button", handleArticleSave);
+    $(document).on("click", "#save-button", handleArticleSave);
     $(document).on("click", "#scrape", handleArticleScrape);
     //     $(".clear").on("click", handleArticleClear);
 
@@ -85,7 +84,7 @@ $(document).ready(function () {
     //     }
 
     function handleArticleSave() {
-
+        console.log("button clicked")
         // This function is triggered when the user wants to save an article
         // When we rendered the article initially, we attached a javascript object containing the headline id
         // to the element using the .data method. Here we retrieve that.
