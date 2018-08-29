80c13c62f990d50ed29a5f44cb0fca42cd69dc45
bkell11 <bkeller11@msn.com>
2018-08-28 20:51:29 -0600

    mongoconnection

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/articledb");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articledb";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.get("/", function (req, res) {
     // get articles saved or all
